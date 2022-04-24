55819d07e752f20e21092c324a56deb8aadaa635
bang-js <95086336+bang-js@users.noreply.github.com>
2022-04-24 23:32:55 +0900

    Update mkt_study.py

--- mkt_study.py
+++ mkt_study.py
@@ -1,3 +1,30 @@
-# STUDY 1
-# 최근 50년 동안 고점을 돌파하지 못한 기간 분석 : 강세장 약세장 분석
-# 이를 spread(credit, rate) 및 이자율(or인상발표시기) 추이와 비교
+# ##########
+# # STUDY 1 : MDD
+# ##########
+# # 이를 spread(credit, rate) 및 이자율(or인상발표시기) 추이와 비교
+
+import pandas as pd
+
+# ETF 선택
+etf_ticker = 'IXIC'
+
+# csv 파일 불러오기
+df = pd.read_csv('C:/etf/{}.csv'.format(etf_ticker))
+
+# 1:Open, 2:H, 3:L, 4:Close
+
+date = []
+dd_list = []
+
+for idx in range(1, df.shape[0]) :
+    m = max(df.iloc[0:idx+1, 4])
+    dd = df.iloc[idx][4]/m - 1
+    dd_list.append(dd)
+    date.append(df.iloc[idx][0])
+
+ROR = pd.DataFrame([ x for x in zip(date, dd_list)])
+ROR.rename(columns={0:'date', 1:'drawdown'}, inplace=True)
+
+filename = '{}_mkt_study_MDD.csv'.format(etf_ticker)
+file = open(filename, "w", encoding="utf-8-sig")  
+ROR.to_csv('C:/etf/'+filename, index=None)
